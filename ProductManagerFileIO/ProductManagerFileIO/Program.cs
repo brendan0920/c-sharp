63354ab6976f7f387321d85f7dd8bd8906252a77
brendan0920 <brdk1220@gmail.com>
2024-09-13 11:42:52 -0400

    updated

--- ProductManagerFileIO/ProductManagerFileIO/Program.cs
+++ ProductManagerFileIO/ProductManagerFileIO/Program.cs
@@ -1,4 +1,7 @@
 ﻿using ProductManagerFileIO.Model;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
 
 namespace ProductManagerFileIO
 {
@@ -7,29 +10,14 @@ namespace ProductManagerFileIO
         static List<Product> products = new();
         
             // Declare file path for use later            
-        static string path = @"c:\files\Names.txt";
+        static string path = @"c:\files\Products.txt";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Product Manager App!");
 
             // LOAD PRODUCTS FROM FILE - into products list
-            using StreamReader textIn = new(new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read));
-            while (textIn.Peek() != -1)
-            {
-                string row = textIn.ReadLine() ?? "";
-                string[] fields = row.Split('|');
-                if (fields.Length == 3)
-                {
-                    string code = fields[0];
-                    string desc = fields[1];
-                    decimal price = Decimal.Parse(fields[2]);
-                    Product p = new Product(code, desc, price);
-                    products.Add(p);
-                }
-            }
-
-
+            LoadProducts();
 
             DisplayMenu();
 
@@ -68,53 +56,90 @@ namespace ProductManagerFileIO
         static void ListProducts()
         {
             Console.WriteLine("\nProduct List:");
-            Console.WriteLine("Method Not Yet Implemented");
+            
+            // do biz
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+                Console.WriteLine();
+            }
+
+
         }
 
         static void GetProduct()
         {
-            //Console.WriteLine("\nGet Product By Code:");
-            //Console.WriteLine("Method Not Yet Implemented");
-
+            Console.WriteLine("\nGet Product By Code:");
             // prompt user for code            
+            string code = GetString("\nEnter Code:");
             // find product in list, by code
-            string getProduct = GetString("\nGet Product By Code:");
-
-
+            Product pdt = null;
+            foreach(Product product in products)
+            {
+                if (product.Code == code)
+                {
+                    pdt = product;
+                }
+            }
             // display product
-            Console.WriteLine($"Product - Code:{getProduct}, Desc:{}, Price: {}");
+            if (pdt != null)
+            {
+                Console.WriteLine(pdt);
+            }else
+            {
+                Console.WriteLine($"Product for code: {code} not found. ");
+            }
+
 
         }
         static void AddProduct()
         {
             // prompt user for code, desc, price
             Console.WriteLine("\nAdd a New Product for code, desc and price:");            
-            // create new instance of product
-            Product addProduct = new Product();
             // add product to list
-            addProduct.Code = GetString("Enter a product code: ");
-            addProduct.Description = GetString("Enter a product description: ");
-            addProduct.Price = GetDecimal("Enter a product price: ", 0.0m, decimal.MaxValue);
-            products.Add(addProduct);
+            string code = GetString("Enter a product code: ");
+            string description = GetString("Enter a product description: ");
+            decimal price = GetDecimal("Enter a product price: ", 0.0m, decimal.MaxValue);
+            // create new instance of product
+            Product p = new Product(code, description, price);
+            products.Add(p);
             // SAVE PRODUCT FILE
-            using StreamWriter textOut = new(new FileStream(path, FileMode.Create, FileAccess.Write));
-            foreach (Product p in products)
-            {
-                textOut.Write(p.Code + " | ");
-                textOut.Write(p.Description + " | ");
-                textOut.Write(p.Price);
-            }
+            SaveProducts();
+            Console.WriteLine($"Product {code} added!\n");
+            
         }
 
         static void DeleteProduct()
         {
             // prompt user for product code
-            Console.WriteLine("\nDelete a Product By Code:");            
+            Console.WriteLine("\nDelete a Product By Code:");
+            // prompt user for code            
+            string code = GetString("\nEnter Code:");
+            // find product in list, by code
+            Product pdt = null;
+            foreach (Product product in products)
+            {
+                if (product.Code == code)
+                {
+                    pdt = product;
+                }
+            }            
+            // if product exists, delete product 
+            if(pdt != null)
+            {
+                products.Remove(pdt);
+                
+                //SAVE PRODUCT FILE
+                SaveProducts();
 
-            // find product by code
-            // if product does not exist, display message
-            // if product exists, delete product and SAVE PRODUCT FILE
-            //
+                Console.WriteLine($"Product {code} removed.\n");
+            }
+            else
+            {
+                Console.WriteLine($"No Product for code {code} exists. \n");
+            }
+
+
         }
 
         static void DisplayMenu()
@@ -130,8 +155,47 @@ namespace ProductManagerFileIO
 
 
         // NEW METHODS:
-        // SAVE PRODUCT FILE
-        // LOAD PRODUCTS FROM FILE
+
+        //  1. SAVE PRODUCT FILE : save our list of products in the products.txt file
+        private static void SaveProducts()
+        {
+        // create a StreamWriter to access fhte file for writing
+            using StreamWriter productsWriter = new(new FileStream(path, FileMode.Create, FileAccess.Write));
+        // loop through the products list
+            foreach (Product product in products)
+            {
+        // - streamwriter.write each field separated by '|', then end with a newline
+                productsWriter.WriteLine($"{product.Code}|{product.Description}|{product.Price}");                
+            }
+        }
+
+
+
+
+
+        //  2. LOAD PRODUCTS FROM FILE        
+        private static void LoadProducts()
+        {
+            // create a StreamReader to access the file
+            using StreamReader productsReader = new(new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read));
+            // read the lines from the file
+            while (productsReader.Peek() != -1)
+            {
+            // parse the fields from each line into a Product
+            // add the Product to the products list
+                string row = productsReader.ReadLine() ?? "";
+                string[] fields = row.Split('|');
+                if (fields.Length == 3)
+                {
+                    string code = fields[0];
+                    string desc = fields[1];
+                    decimal price = Decimal.Parse(fields[2]);
+                    Product p = new Product(code, desc, price);
+                    products.Add(p);
+                }
+            }
+        }
+
 
 
         //GetString
